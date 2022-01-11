895b5f7c8348fdfe75bc8ac2377468cfbdc6aaf1
Shohei <shayakawa@bayviewglen.ca>
2022-01-11 08:36:14 -0500

    characters

---
+++ Final Project/src/zork/characters.java
@@ -0,0 +1,81 @@
+package zork;
+
+public class characters {
+    private int hp;
+  private String name;
+  private String id;
+  private boolean isFightable;
+  private String room;
+
+
+  public characters(){
+    this.hp = 0;
+    this.name = "DEFAULT_NAME";
+    this.isFightable= false;
+    this.id = "";
+    this.room = "DEFAULT_ROOM";
+  }
+  
+  public characters(int hp, String name, boolean isFightable, String id, String description, String room) {
+    this.hp = hp;
+    this.name = name;
+    this.isFightable = isFightable;
+    this.id = id;
+    this.room = room;
+  }
+
+  public characters(int hp, String keyName, boolean isFightable){
+    this.hp = hp;
+    this.name = keyName;
+    this.isFightable = isFightable;
+  }
+
+public void fight() {
+    if (!isFightable)
+      System.out.println("The " + name + " cannot be damaged.");
+  }
+
+  public int gethp() {
+    return hp;
+  }
+
+  public void sethp(int hp) {
+    this.hp = hp;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getId(){
+    return id;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public boolean isFightable() {
+    return isFightable;
+  }
+
+  public void setFightable(boolean isFightable) {
+    this.isFightable = isFightable;
+  }
+
+  public void setId(String id){
+    this.id = id;
+  }
+
+  public void setDescription(String description){
+  }
+
+  public void setroom(String room){
+    this.room = room;
+  }
+
+  public String getroom(){
+    return room;
+  }
+  
+}
